d254ee2db03adaa69c4938f63dbc4d5e2331cf48
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2023-08-06 11:45:31 -0400

    Fix status logic

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ key_custom = bytes([
 ])
 # fmt: on
 
-pd_info = [
+pd_list = [
     # PD_0 info
     {
         "address": 0,
@@ -27,22 +27,18 @@ pd_info = [
 
 
 class OSDPd:
-    PD_0 = 0
-
     @classmethod
-    async def create(cls, pd_info):
+    async def create(cls, pd_list):
         self = OSDPd()
+        self.pd_list = pd_list
 
-        cp = osdp.ControlPanel(pd_info)
+        cp = osdp.ControlPanel(pd_list)
         cp.set_loglevel(osdp.LOG_DEBUG)
         cp.set_event_callback(self.handle_event)
         self.cp = cp
 
         self.loop = asyncio.get_running_loop()
-
-        self.connected = False
-        self.secured = False
-        self.ready = False
+        self.pd_status = 0
 
         await self.refresh()
 
@@ -60,7 +56,7 @@ class OSDPd:
 
     async def process_card_read_event(self, address, event):
         await asyncio.to_thread(utils.send_pending_access_feedback, self.cp, address)
-        await asyncio.sleep(10)  # Simulate slow network call
+        await asyncio.sleep(0.5)  # Simulate slow network call
 
         if event["data"] == b"\x12345":
             await asyncio.to_thread(utils.send_allow_access_feedback, self.cp, address)
@@ -77,25 +73,28 @@ class OSDPd:
         while True:
             self.cp.refresh()
 
-            if self.connected != self.cp.status():
-                self.connected = self.cp.status()
-            if self.secured != self.cp.sc_status():
-                self.secured = self.cp.sc_status()
-            if self.ready != (self.connected and self.secured):
-                self.ready = self.connected and self.secured
-                await self.handle_pd_state_change()
+            pd_status = self.cp.status() & self.cp.sc_status()
+            pd_status_diff = pd_status ^ self.pd_status
+
+            for pd_index in range(len(self.pd_list)):
+                toggled = pd_status_diff & (1 << pd_index)
+                ready = pd_status & (1 << pd_index)
+                if toggled:
+                    await self.handle_pd_state_change(pd_index, ready)
+
+            self.pd_status = pd_status
 
             await asyncio.sleep(0.020)
 
-    async def handle_pd_state_change(self):
-        logging.info(f"PD state changed to: {self.ready}")
-        if self.ready:
-            await asyncio.to_thread(utils.send_idle_feedback, self.cp, OSDPd.PD_0)
+    async def handle_pd_state_change(self, pd_index, ready):
+        logging.info(f"PD {pd_index} state changed to: {ready}")
+        if ready:
+            await asyncio.to_thread(utils.send_idle_feedback, self.cp, pd_index)
 
 
 async def main():
     logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.DEBUG)
-    await OSDPd.create(pd_info)
+    await OSDPd.create(pd_list)
 
 
 if __name__ == "__main__":
